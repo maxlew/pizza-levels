97c5ee087a74657a11b72e10289b177a3f961a9d
Max Lewis <max.lewis@aesop.com>
2024-03-11 11:21:18 +1100

    fix(api): fixing api endpoints and app router issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,44 @@ import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import ErrorComponent from "./error";
 import PizzaController from "./components/pizza-controller";
 
-async function getPizzaData(): Promise<PizzaData[]> {
-  const response = await fetch('http://localhost:3000/api/pizza');
+// If I had more time I'd type this properly or find someone elses
+type PoorlyTypedGoogleSheetRow = {
+  values: {
+    formattedValue: string;
+    effectiveValue: {
+      numberValue: number;
+    }
+  }[]
+}
 
-  if (!response.ok) {
-    throw new Error('Unable to fetch Pizza response from API');
+const getSheet = async (): Promise<PizzaData[]> => {
+  // Note this has an artificial cap at 40 rows due to the range needing to be included
+  // At that point I probably should stop using Google Sheets and use MySQL
+  return fetch(`https://sheets.googleapis.com/v4/spreadsheets/${process.env.PIZZA_SHEET_ID}?key=${process.env.GSHEETS_API_KEY}&includeGridData=true&ranges=A2:D40`)
+    .then(res => res.json())
+    .then(sheet => sheet.sheets[0].data[0].rowData)
+    .then(sheetData => sheetData.map(parseGoogleSheetsRow).filter(Boolean))
+    .catch((err) => {
+      console.error(err);
+      throw new Error('Error requesting data from Google Sheet');
+    })
+}
+
+const parseGoogleSheetsRow = (row: PoorlyTypedGoogleSheetRow): PizzaData | boolean => {
+  if (typeof row.values[0].formattedValue === 'undefined') {
+    // return blank row to be filtered out
+    return false
   }
-
-  const json = await response.json();
-  return json.data;
+  return {
+    restaurant: row.values[0].formattedValue,
+    level: row.values[1].effectiveValue.numberValue,
+    rating: row.values[2].effectiveValue.numberValue,
+    notes: row.values[3].formattedValue,
+  }
 }
 
 export default async function App() {
-  const pizzaData = await getPizzaData();
+  const pizzaData = await getSheet();
 
   return (
     <ErrorBoundary errorComponent={ErrorComponent}>
